afd8b7e9474e53491bfb1dcb5b88d4f8a2a84d28
Hiroki.H <hon87kot12.xp@gmail.com>
2016-11-17 17:18:23 -0800

    Refactoring package name

---
+++ src/java/foundation/example/Chapter3_2.java
@@ -0,0 +1,35 @@
+package java.foundation.example;
+
+/**
+ * Created by hhonda on 2016-11-17.
+ */
+public class Chapter3_2 extends ChapterBase {
+
+    @Override
+    public void main() {
+        // write your code here
+        //System.out.print("Type here 'anytown, USA' : ");
+
+        String college = new String("PoDunk College");
+        String getTown = "Anytown, USA";//new Scanner(System.in).next();
+
+        int stringLength;
+        String change1, change2, change3;
+
+        stringLength = college.length();
+        System.out.println(college + " contains " + stringLength + " characters.");
+
+//        char temp;
+//        for (int i = 0; i < college.length(); i++) {
+//            temp = college.charAt(i);
+//            if (Character.isUpperCase(temp)) {
+//
+//            }
+//        }
+        change1 = college.toUpperCase();
+        change2 = change1.replace("O", "*");
+        change3 = college.concat(getTown);
+
+        System.out.println("The final string is " + change3);
+    }
+}
---
+++ src/java/foundation/example/ChapterBase.java
@@ -0,0 +1,12 @@
+package java.foundation.example;
+
+/**
+ * Created by hhonda on 2016-11-17.
+ */
+public class ChapterBase {
+
+    ChapterBase() {}
+    public void main() {}
+
+    public interface OnCallLinstener { void onCall();}
+}
